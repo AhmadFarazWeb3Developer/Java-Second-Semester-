7115d4a9676e2c5f442b30f0a20075abb25da0ff
Ahmad Faraz <129995311+AhmadFarazWeb3Developer@users.noreply.github.com>
2023-07-10 16:38:35 +0500

    Add files via upload

---
+++ DataTypes/Double.java
@@ -0,0 +1,14 @@
+
+public class Double {
+
+public static void main (String[] args)	
+{
+	
+	
+	double a=30;
+	double b=30;
+	double division=a/b;
+	System.out.println(division);
+}
+	
+}
---
+++ DataTypes/Float.java
@@ -0,0 +1,15 @@
+
+public class Float {
+
+	public static void main(String[] args )
+	{
+		
+		float a=30;
+		float b=45;
+		float mod=a%b;
+		
+	System.out.println("Modulus of a and b : "+mod);
+	}
+	
+	
+}
